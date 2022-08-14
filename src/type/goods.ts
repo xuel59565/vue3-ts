37fa64f7cadb860639e24d0768405976b1758b9e
“xuel59565” <1627687057@qq.com>
2022-08-14 23:45:43 +0800

    用户接口

--- src/type/goods.ts
+++ src/type/goods.ts
@@ -1,25 +1,32 @@
-export interface ListInt{
-    userId:number,
-    id:number,
-    title:string,
-    introduce:string
+export interface ListInt {
+    userId: number,
+    id: number,
+    title: string,
+    introduce: string
 
 }
-interface selectDataInt{
-    title:string,
-    introduce:string,
-    page:number,//页码
-    count:number, //总条数
-    pagesize:number,//默认一页显示几条
+interface selectDataInt {
+    title: string,
+    introduce: string,
+    page: number,//页码
+    count: number, //总条数
+    pagesize: number,//默认一页显示几条
 
 }
-export class InitData{
-    selectData:selectDataInt={
-        title:"",
-        introduce:"",
-        page:1,
-        count:0,
-        pagesize:5,
+//定义一个接收角色的接口
+interface RoleListInt {
+    authority: number[]
+    roleId: number
+    roleName: string
+}
+export class InitData {
+    selectData: selectDataInt = {
+        title: "",
+        introduce: "",
+        page: 1,
+        count: 0,
+        pagesize: 5,
     }
-    list:ListInt[]=[] //展示的内容的数据,接受从后台返回的数据
+    list: ListInt[] = [] //展示的内容的数据,接受从后台返回的数据
+    roleList: RoleListInt[] = []  //接收角色信息的列表
 }
