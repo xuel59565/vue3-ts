d77ccadd97ae131062e7af97bec6882918b216c0
“xuel59565” <1627687057@qq.com>
2022-08-16 00:12:39 +0800

    角色

--- src/type/user.ts
+++ src/type/user.ts
@@ -5,21 +5,26 @@ export interface ListInt {
     role: RoleInt[]
     userName: string
 }
-interface RoleInt{
-    role:number,
-    roleName:string
+interface RoleInt {
+    role: number,
+    roleName: string
 }
 // 定义一个接口，搜索的
-interface SelectDataInt{
+interface SelectDataInt {
     //需要查询的东西
-    role:number,
-    nickName:string
-
+    role: number,
+    nickName: string
 }
-export class InitData{
-    salectData:SelectDataInt={
-        nickName:"",
-        role:0
+interface RoleListInt {
+    authority: string[]
+    roleId: number
+    roleName: string
+}
+export class InitData {
+    salectData: SelectDataInt = {
+        nickName: "",
+        role: 0
     }
-    list:ListInt[]=[]//用来接收用户信息的列表
+    list: ListInt[] = []//用来接收用户信息的列表
+    roleList: RoleListInt[] = []//用来接收角色信息的列表
 }
