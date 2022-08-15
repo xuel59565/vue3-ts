f9698fb44743dc34ef53b8a8902d41cbcb92c803
XL <103798802+xuel59565@users.noreply.github.com>
2022-08-15 16:00:02 +0800

    nothing

--- src/views/GoodsView.vue
+++ src/views/GoodsView.vue
@@ -9,26 +9,31 @@
           <el-input v-model="selectData.introduce" placeholder="请输入关键字" />
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">查询</el-button>
+          <el-button type="primary" >查询</el-button>
         </el-form-item>
       </el-form>
+    </div>
+    <div>
+        <el-table-column prop="date" label="Date" width="180" />
+        <el-table-column prop="name" label="Name" width="180" />
+        <el-table-column prop="address" label="Address" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { throwStatement } from "@babel/types";
+// import { throwStatement } from "@babel/types";
 import { defineComponent, reactive, toRefs } from "vue";
 import { getGoodsList } from "../request/api";
 import { InitData } from "../type/goods";
 export default defineComponent({
   setup() {
-    const data=reactive(new InitData)
+    const data = reactive(new InitData());
     getGoodsList().then((res) => {
       console.log(res);
     });
 
-    return {...toRefs(data)};
+    return { ...toRefs(data) };
   },
 });
 </script>
